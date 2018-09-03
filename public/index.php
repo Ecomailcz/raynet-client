9ff4c52b74fc0583c74311e82e9f45c70b4a4701
Tlapi <jan@ecomail.cz>
2018-09-03 13:06:12 +0200

    remove public folder

--- public/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once('../vendor/autoload.php');
-
-$client = new \EcomailRaynet\Client('sheetpiano@seznam.cz', 'crm-cef529a7397b4648bb852e0696ed0011', 'sheetpiano');
-
-var_dump($client->makeRequest('GET', 'api/v2/person/'));
